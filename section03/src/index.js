a288666e49cfbc9e1bb8a030d98e7078c0a82712
kingof7 <vlfl0@naver.com>
2024-07-06 23:16:22 +0900

    3.5) Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -4,7 +4,10 @@
 // import mul from "./math.js"; // math 모듈의 대표 함수를 지정
 // import { add, sub } from "./math.js"; // ESM, 확장자까지 써준다 .js
 import mul, { add, sub } from "./math.js"; // 대표 + 기타 합쳐서 사용
+import randomColor from "randomcolor"; // 라이브러리에서 갖고옴
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+const color = randomColor();
+console.log(color);
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
