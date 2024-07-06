ed2d2b025c9af749c8952e500f19a2e1361e5d8e
kingof7 <vlfl0@naver.com>
2024-07-07 02:36:31 +0900

    5.2) React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import Header from "./components/Header"; // 확장자 생략 가능, Vite에서 내부적으로 찾도록 설정해둬서
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+// 컴포넌트 시작문자는 항상 대문자
+// 소문자로 쓰면 컴포넌트로 인정 안됨
 
+// App은 부모 컴포넌트(=루트 컴포넌트)
+// Header, Main, Footer는 자식 컴포넌트, 부모 안에 있으니까
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
--- section05/src/main.jsx
+++ section05/src/main.jsx
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// const Hello = () => {
+//   return <div>hello</div>;
+// };
+
+// 루트(=부모) 컴포넌트 변경 예시
+// ReactDOM.createRoot(document.getElementById("root")).render(<Hello />);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
