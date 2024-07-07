efa2cfa62db6e4ebf479b11b05f757a5d84cd27c
jhlee <vlfl0@naver.com>
2024-07-07 15:41:47 +0900

    5.6) State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,30 +1,36 @@
 import "./App.css";
-import Header from "./components/Header"; // 확장자 생략 가능, Vite에서 내부적으로 찾도록 설정해둬서
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
-// 컴포넌트 시작문자는 항상 대문자
-// 소문자로 쓰면 컴포넌트로 인정 안됨
+import { useState } from "react";
 
-// App은 부모 컴포넌트(=루트 컴포넌트)
-// Header, Main, Footer는 자식 컴포넌트, 부모 안에 있으니까
+// state는 각 컴포넌트가 가지고(=내부의)있는 값.
+// state에 따라서 랜더링이 달라짐
+
+// 스테이트의 변화
+// button 클릭 > setState에 의해 state 값이 변함
+// state 변화를 감지하여 App() 함수가 다시 실행되어 화면이 리랜더링 됨
+
+// 리액트에서 리랜더링을 하고 싶다면 useState()를 써서 state값을 관리해야한다.
+
 function App() {
-  const buttonProps = {
-    text : "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0); // 2개의 요소를 담은 배열을 반환
+                             // 첫번째 : 새로운 값 / 두번째 값은 : state를 변화시키는 함수
+  const [light, setLight] = useState("OFF");
+
   return (
     <>
-      {/* <Button text={"메일"} color={"red"}/> */}
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-        <div>자식요소</div>
-      </Button>
+      <div>
+        <h1>{ light }</h1>
+        <button onClick={()=>{
+          setLight(light === 'ON' ? 'OFF' : 'ON');
+        }}>
+          전구 { light === 'ON' ? '끄기' : '켜기' }
+        </button>
+      </div>
+      <div>
+        <h1>{ count }</h1>
+        <button onClick={()=>{
+          setCount(count + 1)
+        }}>+</button>
+      </div>
     </>
   );
 }
