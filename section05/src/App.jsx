78721151b1a07cbc0287eb4b6be4c63a0dc948f2
jhlee <vlfl0@naver.com>
2024-07-07 16:03:58 +0900

    5.7) State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,36 +1,22 @@
 import "./App.css";
-import { useState } from "react";
 
-// state는 각 컴포넌트가 가지고(=내부의)있는 값.
-// state에 따라서 랜더링이 달라짐
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
-// 스테이트의 변화
-// button 클릭 > setState에 의해 state 값이 변함
-// state 변화를 감지하여 App() 함수가 다시 실행되어 화면이 리랜더링 됨
 
-// 리액트에서 리랜더링을 하고 싶다면 useState()를 써서 state값을 관리해야한다.
+
+// 리액트 리렌더링 조건
+// 1. state값이 변할 때,
+// 2. props를 받을 때,
+// 3. 부모 컴포넌트가 리랜더링 될때, 자식도.. --> 성능의 저하를 가져옴
+// -> 스테이트를 부모 함수에 몰어넣지 말고, 함수로 분리해서 거기다가 옮긴다.
+// -> 함수를 분리했기 때문에, state가 변해도 그 함수만 리랜더링 됨. (Bulb, Counter, ..)
 
 function App() {
-  const [count, setCount] = useState(0); // 2개의 요소를 담은 배열을 반환
-                             // 첫번째 : 새로운 값 / 두번째 값은 : state를 변화시키는 함수
-  const [light, setLight] = useState("OFF");
-
   return (
     <>
-      <div>
-        <h1>{ light }</h1>
-        <button onClick={()=>{
-          setLight(light === 'ON' ? 'OFF' : 'ON');
-        }}>
-          전구 { light === 'ON' ? '끄기' : '켜기' }
-        </button>
-      </div>
-      <div>
-        <h1>{ count }</h1>
-        <button onClick={()=>{
-          setCount(count + 1)
-        }}>+</button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
