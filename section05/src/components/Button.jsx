9325a97a73bbf74830f55dfbaf9ed8b4ce4cf30a
jhlee <vlfl0@naver.com>
2024-07-07 15:20:28 +0900

    5.5) 이벤트 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -9,8 +9,17 @@
 
 // let {text, color} = props 객체 분해 할당에 의해
 const Button = ({text, color, children}) => { // text, color에 구조분해 할당, children은 <Button> </Button> 내부 children
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e); // SyntheticBaseEvent = 합성 이벤트 객체 : 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태
+                        // -> 브라우저 마다 규격, 동작방식도 달라서(=크로스브라우징이슈) 합성 이벤트 객체가 생겨남
+        console.log(`${text} - ${color}`);
+    };
     return (
-        <button style={{color: color}}>
+        <button
+        onClick={onClickButton}
+        // onMouseEnter={onClickButton}
+        style={{color: color}}>
             {text} - {color.toUpperCase()}
             {children}
         </button>
