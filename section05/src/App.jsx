5ab78deaf923ff109449deac15c08014288e43b9
kingof7 <vlfl0@naver.com>
2024-07-08 22:50:55 +0900

    5.10) useRef 추가내용

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Register from "./components/Register";
 
 function App() {
+  // 아래 <Register /> 는 그냥 Register 함수를 호출한것 뿐.
+  // 즉 2번 호출한 것.
+  // 두개의 <Register /> 서로다른 countRef.current값을 가짐
   return (
     <>
       <Register />
